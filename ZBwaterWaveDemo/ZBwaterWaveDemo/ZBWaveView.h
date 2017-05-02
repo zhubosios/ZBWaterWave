13c0ff0f64b7032a9e3ff3321b3894c9f41709d3
zhubo <2370233465@qq.com>
2017-05-02 17:29:11 +0800

    添加封面效果图

--- ZBwaterWaveDemo/ZBwaterWaveDemo/ZBWaveView.h
+++ ZBwaterWaveDemo/ZBwaterWaveDemo/ZBWaveView.h
@@ -9,9 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ZBWaveView : UIView
-@property (nonatomic, assign) CGFloat waveSpeed;     // Default as 6
-@property (nonatomic, assign) CGFloat waveAmplitude; // Default as 6
-@property (nonatomic, strong) UIColor   *waveColor; // Default as [UIColor blueColor]
+
+@property (nonatomic, assign) CGFloat waveSpeed;
+@property (nonatomic, assign) CGFloat waveAmplitude;
+@property (nonatomic, strong) UIColor   *waveColor;
 
 - (void)wave;
 - (void)stop;
